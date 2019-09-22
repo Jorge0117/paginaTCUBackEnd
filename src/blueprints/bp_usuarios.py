751e306632a0034b2de60f325c6b4713692ed19f
Jorge Araya <jorgea1177@gmail.com>
2019-09-21 18:52:32 -0600

    CRUD usuarios listo

---
+++ src/blueprints/bp_usuarios.py
@@ -0,0 +1,88 @@
+from flask import Blueprint, jsonify, request
+from ..entities.entity import Session
+from ..entities.usuarios import Usuarios, UsuariosSchema
+
+bp_usuarios = Blueprint('bp_usuarios', __name__)
+
+@bp_usuarios.route('/usuarios')
+def consultar_usuarios():
+    session = Session()
+    objeto_usuarios = session.query(Usuarios).all()
+
+    schema = UsuariosSchema(many=True)
+    usuario = schema.dump(objeto_usuarios)
+
+    session.close()
+    return jsonify(usuario)
+
+@bp_usuarios.route('/usuario-id', methods=['GET'])
+def consultar_usuario_id():
+    correo = request.args.get('correo')
+    session = Session()
+    objeto_usuario = session.query(Usuarios).get(correo)
+
+    schema = UsuariosSchema()
+    usuario = schema.dump(objeto_usuario)
+    session.close()
+    return jsonify(usuario)
+
+@bp_usuarios.route('/usuarios', methods=['POST'])
+def agregar_usuario():
+    # mount exam object
+    posted_usuario = UsuariosSchema(only=('correo', 'nombre', 'apellido1', 'apellido2', 'contrasenna', 'tipo'))\
+        .load(request.get_json())
+
+    usuario = Usuarios(**posted_usuario)
+
+    # persist exam
+    session = Session()
+    session.add(usuario)
+    session.commit()
+
+    # return created exam
+    nuevo_usuario = UsuariosSchema().dump(usuario)
+    session.close()
+    return jsonify(nuevo_usuario), 201
+
+@bp_usuarios.route('/editar-usuario', methods=['POST'])
+def editar_usuario():
+    correo = request.args.get('correo')
+
+    posted_usuario = UsuariosSchema(only=('correo', 'nombre', 'apellido1', 'apellido2', 'contrasenna', 'tipo')) \
+        .load(request.get_json())
+
+    usuario_actualizado = Usuarios(**posted_usuario)
+
+    session = Session()
+    objeto_usuario = session.query(Usuarios).get(correo)
+    if objeto_usuario is None:
+        return "Usuario no encontrado", 404
+
+    schema = UsuariosSchema()
+
+    objeto_usuario.nombre = usuario_actualizado.nombre
+    objeto_usuario.apellido1 = usuario_actualizado.apellido1
+    objeto_usuario.apellido2 = usuario_actualizado.apellido2
+    objeto_usuario.contrasenna = usuario_actualizado.contrasenna
+    objeto_usuario.tipo = usuario_actualizado.tipo
+
+    session.add(objeto_usuario)
+    session.commit()
+    usuario = schema.dump(objeto_usuario)
+    session.close()
+
+    return jsonify(usuario)
+
+@bp_usuarios.route('/eliminar-usuario', methods=['DELETE'])
+def eliminar_usuario():
+    correo = request.args.get('correo')
+    session = Session()
+    objeto_usuario = session.query(Usuarios).get(correo)
+    if objeto_usuario is None:
+        return "Usuario no encontrado", 404
+
+    session.delete(objeto_usuario)
+    session.commit()
+
+    session.close()
+    return 'Usuario eliminado', 200
--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_cors import CORS
 
-from .entities.entity import Session, engine, Base
-from .entities.usuarios import Usuarios, UsuariosSchema
+from .entities.entity import engine, Base
+from .blueprints.bp_usuarios import bp_usuarios
 
 # creating the Flask application
 app = Flask(__name__)
@@ -11,36 +11,5 @@ CORS(app, resources={r"/src/*": {"origins": "http://localhost:4200"}})
 # if needed, generate database schema
 Base.metadata.create_all(engine)
 
+app.register_blueprint(bp_usuarios)
 
-@app.route('/usuarios')
-def get_exams():
-    # fetching from the database
-    session = Session()
-    exam_objects = session.query(Usuarios).all()
-
-    # transforming into JSON-serializable objects
-    schema = UsuariosSchema(many=True)
-    usuario = schema.dump(exam_objects)
-
-    # serializing as JSON
-    session.close()
-    return jsonify(usuario)
-
-
-@app.route('/usuarios', methods=['POST'])
-def add_exam():
-    # mount exam object
-    posted_usuario = UsuariosSchema(only=('correo', 'nombre', 'apellido1', 'apellido2', 'contrasenna', 'tipo'))\
-        .load(request.get_json())
-
-    usuario = Usuarios(**posted_usuario)
-
-    # persist exam
-    session = Session()
-    session.add(usuario)
-    session.commit()
-
-    # return created exam
-    new_exam = UsuariosSchema().dump(usuario)
-    session.close()
-    return jsonify(new_exam), 201
