4e6e5c85f4adf6e5a4636014dee7e5c9d0d52419
Jorge Araya <jorgea1177@gmail.com>
2019-10-03 11:43:52 -0600

    CRUD areas de interes

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from .entities.entity import engine, Base
 from .blueprints.bp_usuarios import bp_usuarios
 from .blueprints.bp_autenticacion import bp_autenticacion
 from .blueprints.bp_areasDeInteres import bp_areasDeInteres
+from .blueprints.bp_files import bp_files
 
 # creating the Flask application
 app = Flask(__name__)
@@ -20,4 +21,5 @@ Base.metadata.create_all(engine)
 app.register_blueprint(bp_usuarios)
 app.register_blueprint(bp_autenticacion)
 app.register_blueprint(bp_areasDeInteres)
+app.register_blueprint(bp_files)
 
