35122a03f631c76b03eb4b76cbfb36ded5d18dc5
Jorge Araya <jorgea1177@gmail.com>
2019-11-11 11:12:13 -0600

    Agregar artpiculos listo

--- src/blueprints/bp_articulos.py
+++ src/blueprints/bp_articulos.py
@@ -15,3 +15,30 @@ def consultar_articulos_area():
     articulos = schema.dump(objeto_articulos)
     session.close()
     return jsonify(articulos)
+
+@bp_articulos.route('/articulos', methods=['POST'])
+@jwt_required
+def agregar_articulo():
+
+    json_articulo = request.get_json()
+    json_articulo['fecha'] = json_articulo['fecha'].split('T')[0]
+    posted_articulo = ArticulosSchema().load(json_articulo)
+
+    articulo = Articulos(**posted_articulo)
+
+    session = Session()
+
+    articulo_base = session.query(Articulos).get(articulo.id)
+    if articulo_base is not None:
+        session.close()
+        return 'Artículo ya se encuentra en la base', 409
+
+    session.add(articulo)
+    session.flush()
+    nueva_area = ArticulosSchema().dump(articulo)
+    session.commit()
+
+    # return created exam
+
+    session.close()
+    return jsonify(nueva_area), 201
